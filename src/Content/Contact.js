4e5495011c2ffdf5949cebfb5522a402c65f5ab7
AKHIL <74488740+akhilnerella81@users.noreply.github.com>
2021-07-11 14:06:58 +0530

    Uploaded image. (#13)
    Co-authored-by: Aditya Agrawal <adiag1200@gmail.com>

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -2,6 +2,7 @@ import tpoImage from "../images/tpoImage.webp";
 import Ananya from "../images/Ananya.png";
 import Jatin from "../images/webCord2.jpeg";
 import Yashwanth from "../images/Yashwanth.jpg";
+import Akhil from "../images/akhil.jpg";
 import Rohith from "../images/Rohith.jpg";
 
 const WebTeam = [
@@ -109,6 +110,7 @@ const PlacementTeam = [
     name: "Nerella Akhil",
     email: "cs19b1020@iiitr.ac.in",
     phone: "+91 8897073603",
+    image: Akhil
   },
   {
     name: "Yashwanth Vallabhu",
