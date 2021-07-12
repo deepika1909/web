2d258d838e635040b9fa514e26a67405d4b76489
abhijeet <abhijeetwankhade2001@gmail.com>
2021-07-12 15:53:37 +0530

    changed photo

--- src/Content/Contact.js
+++ src/Content/Contact.js
@@ -1,134 +1,135 @@
 import tpoImage from "../images/tpoImage.webp";
 import Ananya from "../images/Ananya.png";
+import Abhijeet from "../images/Abhijeet.jpg";
 import Jatin from "../images/webCord2.jpeg";
 import Yashwanth from "../images/Yashwanth.jpg";
 import Akhil from "../images/akhil.jpg";
 import Rohith from "../images/Rohith.jpg";
 
 const WebTeam = [
-  {
-    name: "Abhijeet Wankhade",
-    email: "cs19b1028@iiitr.ac.in",
-    phone: "+91 7720998898",
-    image: "https://cdn.discordapp.com/attachments/863338968048336896/863485872137830451/83881468_2606122862968346_7668919577576813518_n.png"
-  },
-  {
-    name: "Jatin Sachdeva",
-    email: "cs19b1013@iiitr.ac.in",
-    phone: "+91 9068334677",
-    image: Jatin,
-  },
-  {
-    name: "Aditya Agrawal",
-    email: "cs19b1003+tnp@iiitr.ac.in",
-    phone: "+91 9785559273",
-    image: "https://bit.ly/crossphoton-img",
-  },
-  {
-    name: "Kushagra Indurakhya",
-    email: "cs19b1017@iiitr.ac.in",
-    phone: "+91 9026409246",
-    image: "https://cdn.kindurkhya.me/profile.jpg"
-  },
+    {
+        name: "Abhijeet Wankhade",
+        email: "cs19b1028@iiitr.ac.in",
+        phone: "+91 7720998898",
+        image: Abhijeet,
+    },
+    {
+        name: "Jatin Sachdeva",
+        email: "cs19b1013@iiitr.ac.in",
+        phone: "+91 9068334677",
+        image: Jatin,
+    },
+    {
+        name: "Aditya Agrawal",
+        email: "cs19b1003+tnp@iiitr.ac.in",
+        phone: "+91 9785559273",
+        image: "https://bit.ly/crossphoton-img",
+    },
+    {
+        name: "Kushagra Indurakhya",
+        email: "cs19b1017@iiitr.ac.in",
+        phone: "+91 9026409246",
+        image: "https://cdn.kindurkhya.me/profile.jpg",
+    },
 ];
 
 const PlacementHead = [
-  {
-    name: "Dr. Sadhna Jha",
-    email: "fic.placement@iiitr.ac.in",
-    phone: "+91 8768779475",
-    image: tpoImage,
-  },
+    {
+        name: "Dr. Sadhna Jha",
+        email: "fic.placement@iiitr.ac.in",
+        phone: "+91 8768779475",
+        image: tpoImage,
+    },
 ];
 
 const PlacementCoordinators = [
-  {
-    name: "Aditya Agrawal",
-    email: "cs19b1003+tnp@iiitr.ac.in",
-    phone: "+91 9785559273",
-    image: "https://bit.ly/crossphoton-img",
-  },
-  {
-    name: "Reethu",
-    email: "cs19b1024@iiitr.ac.in",
-    phone: "+91 8919239558",
-    image: "https://iiitr.ac.in/assets/images/prteam/reethu.webp",
-  },
+    {
+        name: "Aditya Agrawal",
+        email: "cs19b1003+tnp@iiitr.ac.in",
+        phone: "+91 9785559273",
+        image: "https://bit.ly/crossphoton-img",
+    },
+    {
+        name: "Reethu",
+        email: "cs19b1024@iiitr.ac.in",
+        phone: "+91 8919239558",
+        image: "https://iiitr.ac.in/assets/images/prteam/reethu.webp",
+    },
 ];
 
 const PlacementTeam = [
-  {
-    name: "Kushagra Indurakhya",
-    email: "cs19b1017@iiitr.ac.in",
-    phone: "+91 9026409246",
-    image: "https://cdn.kindurkhya.me/profile.jpg",
-  },
-  {
-    name: "Abhishek Sahoo",
-    email: "cs19b1001@iiitr.ac.in",
-    phone: "+91 9358316992",
-  },
-  {
-    name: "Siddharth Saini",
-    email: "cs19b1024@iiitr.ac.in",
-    phone: "+91 9996130681",
-  },
-  {
-    name: "Illa Sai Deepika",
-    email: "cs19b1011@iiitr.ac.in",
-    phone: "+91 9390075787",
-  },
-  {
-    name: "Kaustubh Kesharwani",
-    email: "cs19b1015@iiitr.ac.in",
-    phone: "+91 9121066021",
-    image: "https://drive.google.com/u/0/uc?id=1Wu-b_9d6GU4EagSS1-NDN0QQwrXB2NuB&export=download",
-  },
-  {
-    name: "Manav Chaudhary",
-    email: "cs19b1018@iiitr.ac.in",
-    phone: "+91 7217628148",
-  },
-  {
-    name: "Vibhanshu Jain",
-    email: "cs19b1027@iiitr.ac.in",
-    phone: "+91 9680017462",
-    image: "https://iiitr.ac.in/assets/images/student/gensec_1.jpeg"
-  },
-  {
-    name: "Sai Yaminiee Ganda",
-    email: "cs19b1022@iiitr.ac.in",
-    phone: "+91 7779021200",
-  },
-  {
-    name: "Dosawada Rohith",
-    email: "cs19b1008@iiitr.ac.in",
-    phone: "+91 9394445259",
-    image: Rohith,
-  },
-  {
-    name: "Nerella Akhil",
-    email: "cs19b1020@iiitr.ac.in",
-    phone: "+91 8897073603",
-    image: Akhil
-  },
-  {
-    name: "Yashwanth Vallabhu",
-    email: "cs19b1030@iiitr.ac.in",
-    phone: "+91 9390230911",
-    image: Yashwanth
-  },
-  {
-    name: "Nimish Vadlamudi",
-    email: "cs19b1026@iiitr.ac.in",
-    phone: "+91 8106092571",
-  },
-  {
-    name: "Ananya Mantravadi",
-    email: "cs19b1004@iiitr.ac.in",
-    phone: "+91 9391307112",
-    image: Ananya,
-  },
+    {
+        name: "Kushagra Indurakhya",
+        email: "cs19b1017@iiitr.ac.in",
+        phone: "+91 9026409246",
+        image: "https://cdn.kindurkhya.me/profile.jpg",
+    },
+    {
+        name: "Abhishek Sahoo",
+        email: "cs19b1001@iiitr.ac.in",
+        phone: "+91 9358316992",
+    },
+    {
+        name: "Siddharth Saini",
+        email: "cs19b1024@iiitr.ac.in",
+        phone: "+91 9996130681",
+    },
+    {
+        name: "Illa Sai Deepika",
+        email: "cs19b1011@iiitr.ac.in",
+        phone: "+91 9390075787",
+    },
+    {
+        name: "Kaustubh Kesharwani",
+        email: "cs19b1015@iiitr.ac.in",
+        phone: "+91 9121066021",
+        image: "https://drive.google.com/u/0/uc?id=1Wu-b_9d6GU4EagSS1-NDN0QQwrXB2NuB&export=download",
+    },
+    {
+        name: "Manav Chaudhary",
+        email: "cs19b1018@iiitr.ac.in",
+        phone: "+91 7217628148",
+    },
+    {
+        name: "Vibhanshu Jain",
+        email: "cs19b1027@iiitr.ac.in",
+        phone: "+91 9680017462",
+        image: "https://iiitr.ac.in/assets/images/student/gensec_1.jpeg",
+    },
+    {
+        name: "Sai Yaminiee Ganda",
+        email: "cs19b1022@iiitr.ac.in",
+        phone: "+91 7779021200",
+    },
+    {
+        name: "Dosawada Rohith",
+        email: "cs19b1008@iiitr.ac.in",
+        phone: "+91 9394445259",
+        image: Rohith,
+    },
+    {
+        name: "Nerella Akhil",
+        email: "cs19b1020@iiitr.ac.in",
+        phone: "+91 8897073603",
+        image: Akhil,
+    },
+    {
+        name: "Yashwanth Vallabhu",
+        email: "cs19b1030@iiitr.ac.in",
+        phone: "+91 9390230911",
+        image: Yashwanth,
+    },
+    {
+        name: "Nimish Vadlamudi",
+        email: "cs19b1026@iiitr.ac.in",
+        phone: "+91 8106092571",
+    },
+    {
+        name: "Ananya Mantravadi",
+        email: "cs19b1004@iiitr.ac.in",
+        phone: "+91 9391307112",
+        image: Ananya,
+    },
 ];
 
 export { WebTeam, PlacementCoordinators, PlacementHead, PlacementTeam };
--- src/components/ContactUsCard.jsx
+++ src/components/ContactUsCard.jsx
@@ -3,26 +3,26 @@ import Demo from "../images/contactUsDemoImage.jpg";
 import "../SASS/_contactUs.scss";
 
 function ContactUsCard(props) {
-  return (
-      <div className='contactUsCard'>
-          <img
-              className='contactUsImage'
-              src={props.image || Demo}
-              alt='profile'
-          />
-          <div className='contactUsInfo'>
-              <div>{props.name}</div>
-              <a className='phoneNum' href={`mailto:${props.email}`}>
-                  <i className='fas fa-envelope' aria-hidden='true'></i> :{" "}
-                  {props.email}
-              </a>
-              <a className='phoneNum' href={`tel: ${props.phone}`}>
-                  <i className='fa fa-phone' aria-hidden='true'></i> :{" "}
-                  {props.phone}
-              </a>
-          </div>
-      </div>
-  );
+    return (
+        <div className='contactUsCard'>
+            <img
+                className='contactUsImage'
+                src={props.image || Demo}
+                alt='profile'
+            />
+            <div className='contactUsInfo'>
+                <div>{props.name}</div>
+                <a className='phoneNum' href={`mailto:${props.email}`}>
+                    <i className='fas fa-envelope' aria-hidden='true'></i> :{" "}
+                    {props.email}
+                </a>
+                <a className='phoneNum' href={`tel: ${props.phone}`}>
+                    <i className='fa fa-phone' aria-hidden='true'></i> :{" "}
+                    {props.phone}
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default ContactUsCard;
